0752c0ee089ea70494b27242f65e65a258308e6a
amirn003 <amir.naar@gmail.com>
2023-09-13 22:23:49 +0200

    add create a new yacht

--- app/controllers/yachts_controller.rb
+++ app/controllers/yachts_controller.rb
@@ -4,6 +4,20 @@ class YachtsController < ApplicationController
     @yachts = Yacht.all
   end
 
+  def new
+    @yacht = Yacht.new
+  end
+
+  def create
+    @yacht = Yacht.new(yacht_params)
+    @yacht.user = current_user
+    if @yacht.save
+      redirect_to yacht_path(@yacht)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def my_yachts
     @yachts = Yacht.where(user: current_user)
     render :index
@@ -11,5 +25,11 @@ class YachtsController < ApplicationController
 
   def show
     @yacht = Yacht.find(params[:id])
+  end
+
+  private
+
+  def yacht_params
+    params.require(:yacht).permit(:name, :number_of_guests, :price, :location)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
 
   resources :yachts
-
   get 'my_yachts', to: 'yachts#my_yachts'
 end
