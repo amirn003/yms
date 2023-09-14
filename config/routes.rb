dd891c548278a09e7806999a3c9b93803e8e24e7
amirn003 <amir.naar@gmail.com>
2023-09-14 23:45:45 +0200

    add incomes routes, controller, view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'incomes', to: 'pages#incomes'
 
   resources :yachts, :bookings
   get 'my_yachts', to: 'yachts#my_yachts'
