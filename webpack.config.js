38779c93dfd347ff9638ad978792600432b718cc
amirn003 <amir.naar@gmail.com>
2023-09-19 13:01:58 +0200

    fix assets precompile webpacker configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,46 @@
-const path    = require("path")
-const webpack = require("webpack")
+// const path    = require("path")
+// const webpack = require("webpack")
+
+// module.exports = {
+//   mode: "production",
+//   devtool: "source-map",
+//   entry: {
+//     application: "./app/javascript/application.js"
+//   },
+//   output: {
+//     filename: "[name].js",
+//     sourceMapFilename: "[file].map",
+//     path: path.resolve(__dirname, "app/assets/builds"),
+//   },
+//   plugins: [
+//     new webpack.optimize.LimitChunkCountPlugin({
+//       maxChunks: 1
+//     })
+//   ]
+// }
+
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  mode: "production",
-  devtool: "source-map",
+  mode: "development",
+  devtool: "cheap-module-source-map",
   entry: {
     application: "./app/javascript/application.js"
   },
   output: {
-    filename: "[name].js",
-    sourceMapFilename: "[file].map",
-    path: path.resolve(__dirname, "app/assets/builds"),
+    filename: "application.js",
+    path: path.resolve(__dirname, "public/packs"),
+  },
+  devServer: {
+    writeToDisk: true,
+    publicPath: "/packs/",
+    host: "localhost",
+    port: 3035,
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
     })
   ]
-}
+};
