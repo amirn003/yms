46d6a3c666a1caf3a1419ea092b1da25941ed39e
amirn003 <amir.naar@gmail.com>
2023-09-21 17:05:03 +0200

    add configuration

--- public/packs/application.js
+++ public/packs/application.js
@@ -150,7 +150,10 @@ __webpack_require__.r(__webpack_exports__);
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/amir003/clmsaang502cc01r44xt44gkp"
+      //"mapbox://styles/amir003/clmsafttn02ay01quhr1cdeod"
+      //"mapbox://styles/amir003/clmsaang502cc01r44xt44gkp"
+      //"mapbox://styles/mapbox/streets-v10"
     })
 
     this.#addMarkersToMap()
@@ -158,19 +161,39 @@ __webpack_require__.r(__webpack_exports__);
   }
 
   #addMarkersToMap() {
-    this.markersValue.forEach((marker) => {
+    this.markersValue.forEach((marker, index) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
+
+      // Create a HTML element for your custom marker
+      const customMarker = document.createElement("div")
+      customMarker.innerHTML = marker.marker_html
+      // if (index % 2 === 0) {
+      //   customMarker.innerHTML = marker.marker_html
+      // } else {
+      //   customMarker.innerHTML = marker.marker_html2
+      // }
+
+      const offset = index * 2;
+
+      console.log(marker.lng, marker.lat, marker.location)
+      console.log(offset)
+
+      // Pass the element as an argument to the new marker
+      new mapboxgl.Marker(customMarker, {
+          //draggable: true,
+          offset: [0, offset],
+          occludedOpacity: 0.5
+        }).setLngLat([marker.lng, marker.lat])
         .setPopup(popup)
+        .setPitchAlignment('map')
         .addTo(this.map)
-    });
+    })
   }
 
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 30, duration: 0 })
   }
 
 });
